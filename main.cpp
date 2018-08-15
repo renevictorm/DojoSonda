cb6947a447b45a75f63e890fcc39a4f90f700360
Renê Milagres <renevictorm@gmail.com>
2018-08-14 22:23:21 -0300

    Teste 'testeTamanhoGrid' criado retornando vermelho pois nenhuma implementação ainda foi feita

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+using namespace std;
 
 
 
-void testeTamanhoGrid(){
+bool testeTamanhoGrid(){//teste para a criação do tamanho máximo do grid;
+
+//se o input for menor ou igual a 0 a aplicação deve alterar para no mínimo 1 por 1, caso contrario o teste retorna vermelho(false);  
 	
+	grid G1(2,2);
+	grid G2(-1,0);
+	
+	if (G1.x != 2 || G1.y != 2){// se
+		return = false;
+	} 
+	
+	if (G2.x <= 0 || G2.y <=0){
+		return = false;
+	}
 	
 	
 }
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+
+int main() {
 	return 0;
 }
